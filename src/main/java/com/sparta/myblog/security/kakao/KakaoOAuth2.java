61a59959c671e1a7835dbfdbf88e62c2722dd460
geonyeolpark <msmn1729@gmail.com>
2021-04-02 23:29:37 +0900

    fix: 배포시 소셜로그인 가능하도록 수정

--- src/main/java/com/sparta/myblog/security/kakao/KakaoOAuth2.java
+++ src/main/java/com/sparta/myblog/security/kakao/KakaoOAuth2.java
@@ -30,7 +30,7 @@ public class KakaoOAuth2 {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", "9cae9bc6d9672771328c43c856d73e2d");
-        params.add("redirect_uri", "http://localhost:8080/user/kakao/callback");
+        params.add("redirect_uri", "http://54.180.113.24/user/kakao/callback");
         params.add("code", authorizedCode);
 
         // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
